d83dc87dd96a830d7543c8beda9ee87f95346017
Santiago Hernandez Rangel <124633203+santiagoher@users.noreply.github.com>
2023-04-11 02:54:53 +0000

    Updates

---
+++ ejercicios/ejercicio9.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/*
+    1. Pida 6 numeros en pantalla y los meta a un array
+    2. Mostrar el array enteros, todos sus elementos en el cuerpo de la pagina y en la consola
+    3. Ordenarlo y mostrarlo
+    4. invertir su orden y mostrarlo
+    5. mostrar cuantos elementos tiene el array
+    6. busqueda de un valor introducido por el usuario, y que nos diga si esta y nos diga su 
+*/
